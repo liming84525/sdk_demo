4692360a0e11f94beac382f5e623f6d6ae39614d
liming <liming@tvkou.com>
2016-04-20 12:54:05 +0800

    optimized

--- js/sdk.js
+++ js/sdk.js
@@ -1,28 +1,32 @@
 function Sdk() {
     this.StartMonitor = function(callback) {
-        _startMonitor(callback)
+        __startMonitor(callback)
     }
     this.StopMonitor = function() {
         _stopMonitor()
     }
 
-   function _query() {
-        var result
-        $.ajax({
-            url: "http://btv.tvkou.com:8888/query",
-            dataType: "json",
-            timeout: 2000,
-            success: function(res) {
-                result = res
-            }
-        })
-        return result
-    }
-
     var i
-    function _startMonitor(callback) {
+
+    var oObject = null
+
+    function __startMonitor(callback) {
         i = window.setInterval(function() {
-            callback(_query())
+            $.ajax({
+                url: "http://cntv.tvkou.com:8888/query?time=" + Math.random(),
+                dataType: "json",
+                timeout: 2000,
+                async: false,
+                success: function(res) {
+                    var nObject = res[0]
+                    console.log(nObject.target.id)
+                    console.log(oObject)
+                    if (oObject == null || nObject.target.id !== oObject.target.id) {
+                        callback(nObject)
+                    }
+                    oObject = nObject
+                }
+            })
         }, 1000)
     }
 
